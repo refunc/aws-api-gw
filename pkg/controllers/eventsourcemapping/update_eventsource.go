b8a08451da418b0afcfb5c795a22ae0c9bd76733
Arvin <arvintian8@gmail.com>
2024-07-24 14:29:42 +0800

    add update event source mapping
    Signed-off-by: Arvin <arvintian8@gmail.com>

--- pkg/controllers/eventsourcemapping/update_eventsource.go
+++ pkg/controllers/eventsourcemapping/update_eventsource.go
@@ -1,11 +1,12 @@
 package eventsourcemapping
 
 import (
-	"net/http"
-
 	"github.com/gin-gonic/gin"
+	"github.com/refunc/aws-api-gw/pkg/utils/awsutils"
 )
 
 func UpdateEventSource(c *gin.Context) {
-	c.JSON(http.StatusOK, nil)
+	//No suitable field to carry the payload, please fallback use delete&create to update.
+	//https://docs.aws.amazon.com/lambda/latest/api/API_UpdateEventSourceMapping.html
+	awsutils.AWSErrorResponse(c, 500, "ServiceException")
 }
